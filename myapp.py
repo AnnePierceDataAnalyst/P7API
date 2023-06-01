0c93eb4a3cf917106a1eca01bb3418ab334c2955
Anne <anne@mini-de-admin.home>
2023-06-01 18:37:13 +0200

    avec lime

--- myapp.py
+++ myapp.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 import pandas as pd
 import json
 import pickle
-import lime
+
 
 app = FastAPI()
 
@@ -13,9 +13,8 @@ class Model_Input(BaseModel):
         
 class Model_Target(BaseModel):
     resultat: float
-    features: list
-    importance: list 
-
+ 
+        
 # Où se trouvent les données et le modèle        
 df = pd.read_csv('dataReduced2.csv')
  
@@ -32,26 +31,5 @@ def execute_prediction(input: Model_Input):
     res = model.predict_proba(dfclient)
     res = res[:,0]
     
-    # calculer importances avec Lime
-    feats = [f for f in df.columns if f not in ['TARGET','SK_ID_CURR','SK_ID_BUREAU','SK_ID_PREV']]
-    X = df[feats].values 
-    
-    explainer = lime.lime_tabular.LimeTabularExplainer(X, mode="classification",
-                                              feature_names=feats)
-    
-    idx = df.index[df["SK_ID_CURR"] == idclient2]
-    exp = explainer.explain_instance(X[idx][0], model.predict_proba)
-    
-    # Créer liste de features
-    features = []
-    for i in range(5):
-        features.append (exp.as_list()[i][0])
-        
-    # Créer liste d'importances
-    importance = []
-    for i in range(5):
-        importance.append (exp.as_list()[i][1])        
-
-    
-    return {'resultat': res, 'features':features,'importance': importance}
-
+    return {'resultat': res}
+   
